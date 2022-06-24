629d226e4da64ae0804d48568231bd356ecfe28b
Anastasiadi17 <90448892+Anastasiadi17@users.noreply.github.com>
2022-06-24 23:08:11 +0300

    Add files via upload

--- public/js/proverka_dannih.js
+++ public/js/proverka_dannih.js
@@ -4,9 +4,8 @@ var select = document.getElementById("doki");
 var val = select.value;
 
 if (document.forms[0].vozrast_klienta.value == "" || val=="nol") {
-alert('Необходимо заполнить поле возраст и/или выбрать ответ на результат проверки документов');
-document.mailform.vozrast_klienta.focus();
-return false
+	document.getElementById("err").innerHTML = "Необходимо заполнить поле возраст и/или выбрать ответ на результат проверки документов";
+	return false
 }
 
 return true;
