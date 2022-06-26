47a90468d532cb2801b3a81576b633851cef4ee7
Anastasiadi17 <90448892+Anastasiadi17@users.noreply.github.com>
2022-06-26 12:27:25 +0300

    Add files via upload

--- public/js/proverka_dannih.js
+++ public/js/proverka_dannih.js
@@ -1,7 +1,12 @@
 function sendform() {
 
-var select = document.getElementById("doki");
-var val = select.value;
+let select = document.getElementById("doki");
+let val = select.value;
+
+if ((document.forms[0].vozrast_klienta.value < 1) || (document.forms[0].vozrast_klienta.value > 150)){
+	document.getElementById("err").innerHTML = "Неверно указан возраст!";
+	return false
+}
 
 if (document.forms[0].vozrast_klienta.value == "" || val=="nol") {
 	document.getElementById("err").innerHTML = "Необходимо заполнить поле возраст и/или выбрать ответ на результат проверки документов";
