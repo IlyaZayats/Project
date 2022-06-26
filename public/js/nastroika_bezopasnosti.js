515246dfa73ce8eb9ea8876359915228179e8b73
Anastasiadi17 <90448892+Anastasiadi17@users.noreply.github.com>
2022-06-26 12:28:08 +0300

    Add files via upload

--- public/js/nastroika_bezopasnosti.js
+++ public/js/nastroika_bezopasnosti.js
@@ -1,6 +1,10 @@
+document.getElementById("pass").innerHTML = "";
+document.getElementById("pass1").innerHTML = "";
+document.getElementById("pass2").innerHTML = "";
+
 function checkmailandpass()
 {
-var txt = document.form_pr.address.value;
+let txt = document.form_pr.address.value;
 if(txt.length>0){
 	if (txt.indexOf(".") == -1) {
 		document.getElementById("ema").innerHTML = "Нет символа\".\" в поле адреса новой электронной почты";
@@ -25,22 +29,28 @@ if(txt.length>0){
 		}
 }
 
-var p = document.form_pr.tek_pass.value;
-var p1 = document.form_pr.new_pass.value;
-var p2 = document.form_pr.new_pass_povt.value;
+let p = document.form_pr.tek_pass.value;
+let p1 = document.form_pr.new_pass.value;
+let p2 = document.form_pr.new_pass_povt.value;
 
 	if((p1.length>0 || p2.length>0) && (p.length == 0)){
 		document.getElementById("pass").innerHTML = "Вы не ввели действующий пароль!";
+		document.getElementById("pass1").innerHTML = "";
+		document.getElementById("pass2").innerHTML = "";
 		return false;
 	}
 	
 	if((p1.length==0 || p2.length==0) && (p.length > 0)){
 		if(p1==""){
 			document.getElementById("pass1").innerHTML = "Вы не заполнили поле для нового пароля!";
+			document.getElementById("pass").innerHTML = "";
+			document.getElementById("pass2").innerHTML = "";			
 			return false;
 		}
 			if(p2==""){
 			document.getElementById("pass2").innerHTML = "Вы не заполнили поле для повторного пароля!";
+			document.getElementById("pass").innerHTML = "";
+			document.getElementById("pass1").innerHTML = "";
 			return false;
 		}
 	}
@@ -48,14 +58,20 @@ var p2 = document.form_pr.new_pass_povt.value;
 	if(p1.length>0 || p2.length>0){
 		if(p1==""){
 			document.getElementById("pass1").innerHTML = "Вы не заполнили поле для нового пароля!";
+			document.getElementById("pass").innerHTML = "";
+			document.getElementById("pass2").innerHTML = "";
 			return false;
 		}
 			if(p2==""){
 			document.getElementById("pass2").innerHTML = "Вы не заполнили поле для повторного пароля!";
+			document.getElementById("pass").innerHTML = "";
+			document.getElementById("pass1").innerHTML = "";
 			return false;
 		}
 		if(p1!=p2){
 			document.getElementById("pass2").innerHTML = "Пароли не совпадают";
+			document.getElementById("pass").innerHTML = "";
+			document.getElementById("pass1").innerHTML = "";
 			return false;
 		}
 	}
